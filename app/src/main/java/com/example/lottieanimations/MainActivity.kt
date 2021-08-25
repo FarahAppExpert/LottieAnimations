d27da613ca7da2ed377264aebc8401979a721be0
Farah <Farah.Salih@natwest.com>
2021-08-25 19:18:21 +0100

    Lottie Animation

---
+++ app/src/main/java/com/example/lottieanimations/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lottieanimations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.airbnb.lottie.LottieAnimationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var lottieAnimationView: LottieAnimationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lottieAnimationView = findViewById(R.id.lottieAnimationView)
+
+    }
+}
